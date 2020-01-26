94585cac37c23dd71283b8ddb64515ce2620d636
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:26:30 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,16 +5,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-PACKER_BINARY_PATH = '/usr/local/bin/packer'
-
 
 def test_packer_binary_exists(host):
-    host.file('PACKER_BINARY_PATH').exists
+    assert host.file('/usr/local/bin/packer').exists
 
 
 def test_packer_binary_file(host):
-    host.file('PACKER_BINARY_PATH').is_file
+    assert host.file('/usr/local/bin/packer').is_file
 
 
-def test_packer_binary_run(host):
-    host.check_output('whereis packer') == PACKER_BINARY_PATH
+def test_packer_binary_which(host):
+    assert host.check_output('which packer') == '/usr/local/bin/packer'
